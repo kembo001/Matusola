d21f85deaef90f3d823912ac39abf363d157eeb0
userName <jre1622@gmail.com>
2025-01-20 07:50:16 +0700

    x

--- server.js
+++ server.js
@@ -35,19 +35,19 @@ app.get("/sitemap.xml", (req, res) => {
   res.send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://wholesalecarsmn.com/</loc>
+    <loc>https://www.wholesalecarsmn.com/</loc>
     <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://wholesalecarsmn.com/inventory</loc>
+    <loc>https://www.wholesalecarsmn.com/inventory</loc>
     <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://wholesalecarsmn.com/calculator</loc>
+    <loc>https://www.wholesalecarsmn.com/calculator</loc>
     <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.7</priority>
